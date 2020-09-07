5eff7fb455065ca15704927343b901a512f93ad8
yichuan <cha.po@163.com>
2020-09-07 11:25:37 +0800

    屏幕适配

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,45 @@
-
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'fit/index.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
- 
+
   @override
   Widget build(BuildContext context) {
-     //物理分辨率 physicalSize
-    MyBoxFit.initatal();
-  //手机屏幕大小
-  // MediaQuery.of(context).size.width
+    //物理分辨率 physicalSize
+    MyBoxFit.initatal(standardSize: 750);
+    //手机屏幕大小
+    // MediaQuery.of(context).size.width
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
   @override
   Widget build(BuildContext context) {
-  // final width = MediaQuery.of(context).size.width;
-  // final height = MediaQuery.of(context).size.height;
-  // final padding = MediaQuery.of(context).padding.top;
-  // print(width);
-  // print(height);
-  // print(padding);
-  print(MyBoxFit.width);
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('rpx'),
       ),
       body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize:30),
-            ),
-          ],
-        ),
+        child: Container(
+            width: MyBoxFit.setRpx(400), //400物理像素 750*1334 分辨率
+            height: MyBoxFit.setPx(200), //200pt 逻辑像素 375 * 667 尺寸
+            color: Colors.red,
+            child: Text('Hello')),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
